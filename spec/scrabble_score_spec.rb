4028f09d0eaeb6dc26c4220d375a1732d9958ecb
Daniel Merys <dkmerys@gmail.com>
2020-05-05 10:53:49 -0700

    update class properties and methods

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,7 +4,8 @@ require('pry')
 
 describe('#scrabble_score') do
   it("returns a scrabble score for a letter") do
+    scrabble = Scrabble.new()
     expect("a".scrabble()).to(eq(1))
   end
-  
+
 end
