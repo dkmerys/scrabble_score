a332af88b09ad44bcab8337546572221dd08ce3e
Daniel Merys <dkmerys@gmail.com>
2020-05-05 11:25:39 -0700

    successfully pass lowercase/split test

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -14,44 +14,48 @@ class Scrabble
     @score
   end
 
-  def points_calc #Each loop needed
-    letter_values = { 
-      "a" => 1,
-      "b" => 3,
-      "c" => 3, 
-      "d" => 2, 
-      "e" => 1, 
-      "f" => 4, 
-      "g" => 2, 
-      "h" => 4, 
-      "i" => 1, 
-      "j" => 8, 
-      "k" => 5, 
-      "l" => 1, 
-      "m" => 3, 
-      "n" => 1, 
-      "o" => 1, 
-      "p" => 3, 
-      "q" => 10, 
-      "r" => 1, 
-      "s" => 1, 
-      "t" => 1, 
-      "u" => 1, 
-      "v" => 4, 
-      "w" => 4, 
-      "x" => 8, 
-      "y" => 4, 
-      "z" => 10
-  }
-    @word_array.push(@word)
+  def word_array
+    @word_array
   end
-    
-    
-end  
 
-# class Thing {
-#   constructor(asdf) {
-#     this.thing = asdf;
-#     this.myVal = 1;
-#   }
-# }
+  # def points_calc (@word_array)
+  #   letter_values = { 
+  #     "a" => 1,
+  #     "b" => 3,
+  #     "c" => 3, 
+  #     "d" => 2, 
+  #     "e" => 1, 
+  #     "f" => 4, 
+  #     "g" => 2, 
+  #     "h" => 4, 
+  #     "i" => 1, 
+  #     "j" => 8, 
+  #     "k" => 5, 
+  #     "l" => 1, 
+  #     "m" => 3, 
+  #     "n" => 1, 
+  #     "o" => 1, 
+  #     "p" => 3, 
+  #     "q" => 10, 
+  #     "r" => 1, 
+  #     "s" => 1, 
+  #     "t" => 1, 
+  #     "u" => 1, 
+  #     "v" => 4, 
+  #     "w" => 4, 
+  #     "x" => 8, 
+  #     "y" => 4, 
+  #     "z" => 10
+  # }
+  #   if (word.class == number)
+  #     score.push("Please enter a word")
+  #   elsif
+  #   letter_values.each do |key, value|
+      
+  #   @word_array.each do |letter|
+  #     if ([letter] == letter_values[key])
+  #       score += [key]
+
+  # end
+      
+end  
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,8 +4,7 @@ require('pry')
 
 describe('#scrabble_score') do
   it("returns a scrabble score for a letter") do
-    scrabble = Scrabble.new()
-    expect("a".scrabble()).to(eq(1))
+    scrabble = Scrabble.new("HeLLo")
+    expect(scrabble.word_array).to(eq(["h", "e", "l", "l", "o"]))
   end
-
 end
