1c02a904c0200550a726e132e51262b1ed3ac744
Daniel Merys <dkmerys@gmail.com>
2020-05-05 13:38:42 -0700

    succesfully pass word score test

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -2,60 +2,59 @@ class Scrabble
   
   def initialize(word)
     @word = word.downcase
-    @score = 0
-    @word_array = @word.split(//)
   end
 
   def word
     @word
   end
 
+  def letters
+    word.split('')
+  end
+
   def score
-    @score
+    if (letters.class != String)
+      total = "Please enter a word"
+    else
+      total = 0
+      letters.each do |letter|
+        total += letter_scores[letter]
+      end
+    total
   end
 
-  def word_array
-    @word_array
+  def letter_scores()
+    letter_values = { 
+      "a" => 1,
+      "b" => 3,
+      "c" => 3, 
+      "d" => 2, 
+      "e" => 1, 
+      "f" => 4, 
+      "g" => 2, 
+      "h" => 4, 
+      "i" => 1, 
+      "j" => 8, 
+      "k" => 5, 
+      "l" => 1, 
+      "m" => 3, 
+      "n" => 1, 
+      "o" => 1, 
+      "p" => 3, 
+      "q" => 10, 
+      "r" => 1, 
+      "s" => 1, 
+      "t" => 1, 
+      "u" => 1, 
+      "v" => 4, 
+      "w" => 4, 
+      "x" => 8, 
+      "y" => 4, 
+      "z" => 10
+  }
   end
-
-  # def points_calc (@word_array)
-  #   letter_values = { 
-  #     "a" => 1,
-  #     "b" => 3,
-  #     "c" => 3, 
-  #     "d" => 2, 
-  #     "e" => 1, 
-  #     "f" => 4, 
-  #     "g" => 2, 
-  #     "h" => 4, 
-  #     "i" => 1, 
-  #     "j" => 8, 
-  #     "k" => 5, 
-  #     "l" => 1, 
-  #     "m" => 3, 
-  #     "n" => 1, 
-  #     "o" => 1, 
-  #     "p" => 3, 
-  #     "q" => 10, 
-  #     "r" => 1, 
-  #     "s" => 1, 
-  #     "t" => 1, 
-  #     "u" => 1, 
-  #     "v" => 4, 
-  #     "w" => 4, 
-  #     "x" => 8, 
-  #     "y" => 4, 
-  #     "z" => 10
-  # }
-  #   if (word.class == number)
-  #     score.push("Please enter a word")
-  #   elsif
-  #   letter_values.each do |key, value|
-      
-  #   @word_array.each do |letter|
-  #     if ([letter] == letter_values[key])
-  #       score += [key]
-
-  # end
       
 end  
+# Ask user for a word 
+# Take in user input
+# return users word & score
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,8 +3,13 @@ require('scrabble_score')
 require('pry')
 
 describe('#scrabble_score') do
-  it("returns a scrabble score for a letter") do
+  it("splits a user-inputted word into individual letters") do
     scrabble = Scrabble.new("HeLLo")
-    expect(scrabble.word_array).to(eq(["h", "e", "l", "l", "o"]))
+    expect(scrabble.letters).to(eq(["h", "e", "l", "l", "o"]))
+  end
+
+  it("pulls a value from the letter_scores hash corresponding to the user-inputted key") do
+    scrabble = Scrabble.new("Hello")
+    expect(scrabble.score).to(eq(8))
   end
 end
